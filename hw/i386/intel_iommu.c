76df8bceb466ed7a0c82cf2da11fe96b9b432d40
Peter Xu <peterx@redhat.com>
2017-06-13 16:11:03 +0800

    intel_iommu: hmp: allow "-c" for "info iommu"
    New parameter "-c" for it to clear statistics. Other platforms can
    selectively support this (though none yet).
    
    Signed-off-by: Peter Xu <peterx@redhat.com>

--- hw/i386/intel_iommu.c
+++ hw/i386/intel_iommu.c
@@ -3028,6 +3028,7 @@ static void vtd_info_dump(X86IOMMUState *x86_iommu, Monitor *mon,
                           const QDict *qdict)
 {
     IntelIOMMUState *s = INTEL_IOMMU_DEVICE(x86_iommu);
+    bool clear_stats = qdict_get_try_bool(qdict, "clear_stats", false);
 
     DUMP("Version: %d\n", 1);
     DUMP("Cap: 0x%"PRIx64"\n", s->cap);
@@ -3067,6 +3068,10 @@ static void vtd_info_dump(X86IOMMUState *x86_iommu, Monitor *mon,
     DUMP("Misc: next_frr=%d, context_gen=%d, buggy_eim=%d\n",
          s->next_frcd_reg, s->context_cache_gen, s->buggy_eim);
     DUMP("      iotlb_size=%d\n", g_hash_table_size(s->iotlb));
+
+    if (clear_stats) {
+        vtd_reset_stats(s);
+    }
 }
 #undef DUMP
 
